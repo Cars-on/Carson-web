3ee0600afe1ef2e19bafe15df8eb5746832fd762
leonardomessias98 <leonardomessias98@gmail.com>
2021-09-14 00:35:42 -0300

    feat: :sparkles: creating hero section

--- src/modules/Home/index.tsx
+++ src/modules/Home/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+import Hero from '@/shared/components/molecules/Hero';
 
 import { Container, Content } from './styles';
 
 const Home = () => (
   <Container>
+    <Hero />
+
     <Content />
   </Container>
 );
