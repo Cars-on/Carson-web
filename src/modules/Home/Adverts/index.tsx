8c99371a8ec74d0caf4cfa0e94066d7a8360a16a
MatheusCampos-450 <matheus.campos14@fatec.sp.gov.br>
2021-09-19 20:12:53 -0300

    feat: :sparkles: initial code of announcements

--- src/modules/Home/Adverts/index.tsx
+++ src/modules/Home/Adverts/index.tsx
@@ -1,48 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import CardAd from '@/shared/components/molecules/CardAd';
 import FilterSelected from '@/shared/components/molecules/FilterSelected';
 
 import { Container, Content } from './styles';
 import useSidebarFilter from '@/shared/hooks/useSidebarFilter';
+import { api } from '@/shared/providers/api';
 
 const Adverts: React.FC = () => {
+  const [announcement, setAnnouncement] = useState([{}]);
+
   const { filtersValue } = useSidebarFilter();
-  console.log(filtersValue);
+
+  useEffect(() => {
+    async function getAnnouncement() {
+      try {
+        const response = await api.get('/announcements?page=1&per_page=50');
+
+        setAnnouncement(response.data);
+      } catch (error: any) {
+        console.log(error?.response);
+      }
+    }
+
+    getAnnouncement();
+  }, [api, setAnnouncement]);
+
+  console.log(announcement);
 
   return (
     <Container>
       {filtersValue.length && <FilterSelected />}
-
-      <Content>
-        <CardAd />
-
-        <CardAd />
-
-        <CardAd />
-
-        <CardAd />
-
-        <CardAd />
-
-        <CardAd />
-
-        <CardAd />
-
-        <CardAd />
-
-        <CardAd />
-
-        <CardAd />
-
-        <CardAd />
-
-        <CardAd />
-
-        <CardAd />
-
-        <CardAd />
-      </Content>
+      <Content></Content>
     </Container>
   );
 };
