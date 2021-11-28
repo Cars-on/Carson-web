c2827bf1c9efacfa6d93dbf507852c17ecf88ffd
LeonardoMessias98 <messiasleonardo98@gmail.com>
2021-11-28 20:18:59 -0300

    feat: :sparkles: implementing chat in plataform

--- src/modules/Chat/index.tsx
+++ src/modules/Chat/index.tsx
@@ -1,14 +1,222 @@
-import React from 'react';
-import Conversations from './components/Conversations';
-import Messages from './components/Messages';
+import React, { useEffect } from 'react';
+import NextHead from 'next/head';
+import io from 'socket.io-client';
 
-import { Container } from './styles';
+import { Container, Content, Footer, Messages } from './styles';
+import moment from 'moment';
 
 const ChatModule: React.FC = () => {
+  const socket = io('http://localhost:9999');
+
+  let idChatRoom = '';
+
+  function onLoad() {
+    const urlParams = new URLSearchParams(window.location.search);
+    const name = urlParams.get('name');
+    const email = urlParams.get('email');
+
+    const defaultAvatar =
+      'https://www.seekpng.com/png/detail/428-4287240_no-avatar-user-circle-icon-png.png';
+
+    document.querySelector('.user_logged').innerHTML += `
+    <img
+      className="avatar_user_logged"
+      src=${defaultAvatar}
+    />
+    <strong id="user_logged">${name}</strong>
+  `;
+
+    socket.on('new_users', user => {
+      const existInDiv = document.getElementById(`user_${user._id}`);
+
+      if (!existInDiv) {
+        addUser(user);
+      }
+    });
+
+    socket.emit('get_users', users => {
+      users.map(user => {
+        if (user.email !== email) {
+          addUser(user);
+        }
+      });
+    });
+
+    socket.on('message', data => {
+      if (data.message.roomId === idChatRoom) {
+        addMessage(data);
+      }
+    });
+
+    socket.on('notification', data => {
+      const user = document.getElementById(`user_${data.from._id}`);
+      if (data.roomId !== idChatRoom) {
+        user.insertAdjacentHTML(
+          'afterbegin',
+          `
+      <div className="notification"></div>`,
+        );
+      }
+    });
+  }
+
+  function addMessage(data) {
+    const divMessageUser = document.getElementById('message_user');
+
+    divMessageUser.innerHTML += `
+      <span className="user_name user_name_date">
+       <img
+        className="img_user"
+        src=${data.user.avatar}
+      />
+
+      <strong> ${data.user.name} &nbsp; </strong>
+      <span>  ${moment(data.message.created_at).format(
+        'DD/MM/YYYY HH:mm',
+      )} </span></span
+    >
+    <div className="messages">
+      <span className="chat_message"> ${data.message.text}</span>
+    </div>
+  `;
+  }
+
+  function addUser(user) {
+    const usersList = document.getElementById('users_list');
+    usersList.innerHTML += `
+    <li
+      className="user_name_list"
+      id="user_${user._id}"
+      idUser="${user._id}"
+    >
+      <img
+        className="nav_avatar"
+        src=${user.avatar}
+      />
+      ${user.name}
+    </li>
+  `;
+  }
+
+  useEffect(() => {
+    document.getElementById('user_message').addEventListener('keypress', e => {
+      if (e.key === 'Enter') {
+        const message = e.target.value;
+
+        e.target.value = '';
+
+        const data = {
+          message,
+          idChatRoom,
+        };
+
+        socket.emit('message', data);
+      }
+    });
+
+    document.getElementById('users_list').addEventListener('click', e => {
+      console.log('document');
+
+      const inputMessage = document.getElementById('user_message');
+      inputMessage.classList.remove('hidden');
+
+      document
+        .querySelectorAll('li.user_name_list')
+        .forEach(item => item.classList.remove('user_in_focus'));
+
+      document.getElementById('message_user').innerHTML = '';
+
+      if (e.target) {
+        const idUser = e.target.getAttribute('idUser');
+
+        e.target.classList.add('user_in_focus');
+
+        const notification = document.querySelector(
+          `#user_${idUser} .notification`,
+        );
+
+        if (notification) {
+          notification.remove();
+        }
+
+        socket.emit('start_chat', { idUser }, response => {
+          idChatRoom = response.room.idChatRoom;
+
+          response.messages.forEach(message => {
+            const data = {
+              message,
+              user: message.to,
+            };
+
+            addMessage(data);
+          });
+        });
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    const defaultAvatar =
+      'https://www.seekpng.com/png/detail/428-4287240_no-avatar-user-circle-icon-png.png';
+    const urlParams = new URLSearchParams(window.location.search);
+    const name = urlParams.get('name');
+    const email = urlParams.get('email');
+    const ad_owner_name = urlParams.get('ad_owner_name');
+    const ad_owner_email = urlParams.get('ad_owner_email');
+
+    socket.emit('start', {
+      name,
+      avatar: defaultAvatar,
+      email,
+    });
+
+    if (ad_owner_email && ad_owner_name) {
+      socket.emit('start', {
+        name: ad_owner_name,
+        avatar: defaultAvatar,
+        email: ad_owner_email,
+      });
+    }
+
+    onLoad();
+  }, []);
+
   return (
     <Container>
-      <Conversations />
-      <Messages />
+      <NextHead>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.1.2/tailwind.min.css"
+          integrity="sha512-RntatPOhEcQEA81gC/esYoCkGkL7AYV7TeTPoU+R9zE44/yWxVvLIBfBSaMu78rhoDd73ZeRHXRJN5+aPEK53Q=="
+          crossorigin="anonymous"
+        />
+      </NextHead>
+
+      <Content>
+        <div className="flex flex-1 text-white">
+          <div className="w-3/12 h-screen bg-gray-900 position-relative">
+            <div id="users" className="m-3">
+              <div className="bg-green w-4"></div>
+              <ul id="users_list"></ul>
+            </div>
+
+            <Footer>
+              <div className="user_logged"></div>
+            </Footer>
+          </div>
+
+          <Messages className="w-9/12 bg-gray-700" id="c_message">
+            <div className="message_user" id="message_user"></div>
+
+            <input
+              type="text"
+              id="user_message"
+              className="shadow bg-gray-600 absolute bottom-0 w-8/12 m-3 p-2 rounded focus:outline-none hidden"
+              placeholder="Digite sua mensagem"
+            />
+          </Messages>
+        </div>
+      </Content>
     </Container>
   );
 };
--- src/modules/Chat/styles.ts
+++ src/modules/Chat/styles.ts
@@ -1,12 +1,250 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  display: flex;
-
-  max-width: 100%;
+  margin-top: 90px;
+  height: 100%;
   width: 100%;
-  height: 93vh;
-  padding: 6rem 0 0 0;
+
+  .h-screen.bg-gray-900.position-relative {
+    background: #38bcac;
+    box-shadow: inset 0px 1px #86868675;
+  }
+
+  #users {
+    display: flex;
+    margin-top: 40px;
+  }
+
+  .bg-gray-700 {
+    background: #fff;
+  }
+
+  .shadow.bg-gray-600.absolute.bottom-0.rounded {
+    padding: 1rem;
+    color: #051211;
+    background: white;
+    border-radius: 2rem;
+    filter: drop-shadow(-1.5px 1.5px 3px rgba(0, 0, 0, 0.25));
+  }
+
+  #users_list {
+    li {
+      display: flex;
+      align-items: center;
+
+      cursor: pointer;
+
+      img {
+        max-width: 60px;
+        margin-right: 20px;
+        border-radius: 50%;
+      }
+
+      @media (max-width: 900px) {
+        flex-direction: column;
+        width: 75px;
+        text-align: center;
+
+        img {
+          margin: 20px 0 0 0;
+        }
+
+        &.user_in_focus {
+          padding: 20px 5px;
+        }
+      }
+    }
+  }
+
+  .user_name_list {
+    margin: 1rem 0;
+    padding: 1rem;
+
+    cursor: pointer;
+    color: #051211;
+    background: white;
+    border-radius: 2rem;
+    filter: drop-shadow(-1.5px 1.5px 3px rgba(0, 0, 0, 0.25));
+
+    .user_name_list {
+      display: flex;
+    }
+  }
+
+  .user_name {
+    padding: 10px;
+    display: flex;
+    align-items: center;
+
+    color: #051211;
+  }
+
+  .img_user {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+    margin-right: 10px;
+  }
+
+  .user_name_date {
+    margin-left: 20px;
+  }
+
+  .messages {
+    padding: 10px;
+  }
+
+  .chat_message {
+    display: flex;
+    align-items: center;
+
+    padding: 1rem;
+    color: #051211;
+    background: white;
+    border-radius: 2rem;
+    filter: drop-shadow(-1.5px 1.5px 3px rgba(0, 0, 0, 0.25));
+  }
+
+  .user_logged {
+    padding: 1rem;
+    display: flex;
+    align-items: center;
+
+    border-radius: 2rem;
+    filter: drop-shadow(-1.5px 1.5px 3px rgba(0, 0, 0, 0.25));
+
+    color: #fff;
+
+    img {
+      max-width: 60px;
+      margin-right: 20px;
+      border-radius: 50%;
+    }
+  }
+
+  .chat_message {
+    color: #000;
+  }
+
+  .message_user {
+    overflow-y: hidden;
+    height: max-content;
+
+    div {
+      padding: 0 0 0 80px;
+      margin-bottom: 40px;
+    }
+
+    span {
+      display: flex;
+      align-items: center;
+
+      &,
+      * {
+        color: #000;
+      }
+    }
+
+    img {
+      max-width: 60px;
+      margin-right: 20px;
+      border-radius: 50%;
+    }
+  }
+
+  * {
+    scrollbar-width: thin;
+    scrollbar-color: #374151 #28303b;
+  }
+
+  *::-webkit-scrollbar {
+    width: 12px;
+  }
+
+  *::-webkit-scrollbar-track {
+    background: #374151;
+  }
+
+  *::-webkit-scrollbar-thumb {
+    background-color: #374151;
+    border-radius: 20px;
+    border: 3px solid #28303b;
+  }
+
+  .user_in_focus {
+    background: #237a70;
+    border-radius: 2rem;
+    filter: drop-shadow(-1.5px 1.5px 3px rgba(0, 0, 0, 0.25));
+
+    padding: 0 60px 0 0;
+    color: #fff;
+  }
+
+  .nav_avatar {
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+
+  .user_name_list {
+    display: flex;
+    align-items: center;
+    font-weight: bold;
+  }
+
+  .user_name_list img {
+    margin-right: 10px;
+  }
+
+  .avatar_user_logged {
+    margin-right: 10px;
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+  }
+
+  .notification {
+    width: 20px;
+    height: 20px;
+    background: #ff0101;
+    border-radius: 50%;
+    margin: 3px -4px -27px -11px;
+    border: 2px solid #000;
+    position: absolute;
+  }
+
+  .hidden {
+    display: none;
+  }
+`;
+
+export const Content = styled.section`
+  height: 100%;
+  width: 100%;
 
   position: relative;
+`;
+
+export const Messages = styled.div`
+  position: relative;
+  background: #f7f7f7;
+
+  padding: 20px;
+
+  @media (max-width: 900px) {
+    img {
+      display: none;
+    }
+    .message_user div {
+      padding: 0;
+    }
+  }
+`;
+
+export const Footer = styled.div`
+  position: absolute;
+  height: max-content;
+  bottom: 0;
+
+  background: #38bcac;
 `;
